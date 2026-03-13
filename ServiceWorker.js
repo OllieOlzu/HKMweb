a7c30fdc5a5ad63c5b2fe181b9183abb4d2bf241
OllieOlzu <65995434+OllieOlzu@users.noreply.github.com>
2026-03-14 09:15:41 +1300

    Initial commit

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Hong Kong Mahjong-0.1.0";
+const contentToCache = [
+    "Build/HKM8.loader.js",
+    "Build/HKM8.framework.js",
+    "Build/HKM8.data",
+    "Build/HKM8.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
